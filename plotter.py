cf4a7f1e9e796a1cc2db681d577bb971d5a25ef9
Raymond <raymond.g.chen.22@dartmouth.edu>
2021-09-29 12:05:10 -0400

    Added dashly to project

--- plotter.py
+++ plotter.py
@@ -1,10 +1,17 @@
 #From https://plotly.com/python/
 #and https://towardsdatascience.com/how-to-create-a-plotly-visualization-and-embed-it-on-websites-517c1a78568b
-
+# Used https://blog.finxter.com/your-first-dash-app-how-to-get-started-in-4-minutes-or-less/ to use dashly to display fig rather than just using fig.show()
 import plotly.express as px
 import plotly.io as pio
 from get_data import get_time_series, get_portfolio_value
 import pandas as pd
+
+import dash
+from dash import dcc
+from dash import html
+
+es = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=es)
 
 def generate_chart(portfolio, dates):
     dates = dates.dropna()
@@ -19,8 +26,11 @@ def generate_chart(portfolio, dates):
         dtick="M1",
         tickformat="%b\n%Y",
         ticklabelmode="period")
-    fig.show()
+    app.layout = html.Div(children=[dcc.Graph(figure=fig)])
 
 dates = pd.date_range(start='2020-01-01', end='2020-12-30', freq='D').to_series().dt.dayofweek.where(lambda x : x < 5)
 portfolio = {'MSFT': 0.5, 'AAPL': 0.5}
 generate_chart(portfolio, dates)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
